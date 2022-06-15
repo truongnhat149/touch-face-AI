013b17f0ba7545b4a3cfc068547b91b71362b0a8
Truong <vjc.ichi.149@gmail.com>
2022-06-15 10:23:21 +0700

    dont touch your face

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Getting Started with Create React App
 # Don't Touch Your Face AI 
 
+# Setup
+* cd ../touch-face
+* npm i
+* npm start 
+
+# Guide
+
+* B1: Press the button "Train 1", then do a full half part recording on the person waiting for 5-7s for the camera to record your normal actions
+* B2: Same as step 1: about rotation time and angle. Press the button "Train 2" In this 2nd time, you have to keep your hand on the screen (at least let the camera see your hand) then perform actions to touch your face or move your hand back and forth in front of the camera.
+* B3: Press the button "run"
+
+# quote : f8
--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 
-import { useEffect, useRef } from 'react';
+import { Howl } from 'howler';
+import { useEffect, useRef, useState } from 'react';
+import { initNotifications, notify } from '@mycv/f8-notification';
 import '@tensorflow/tfjs-backend-webgl';
+
 import './App.css';
-// import { Howl } from 'howler';
+import soundHand from './assets/hey_sondn.mp3';
 
-// import soundHand from './assets/hey_sondn.mp3';
-
-const tf = require('@tensorflow/tfjs');
 const mobilenetModule = require('@tensorflow-models/mobilenet');
 const knnClassifier = require('@tensorflow-models/knn-classifier');
-
-// var sound = new Howl({
-//   src: [soundHand]
-// });
-
-// sound.play();
 
 const NOT_TOUCH_LABEL = 'not_touch';
 const TOUCHED_LABEL = 'touched';
 const TRAINING_TIMES = 50;
+const TOUCHED_CONFIDENCE = 0.8;
+
+var sound = new Howl({
+  src: [soundHand]
+});
 
 function App() {
   const video = useRef();
   const mobilenet = useRef();
   const classifier = useRef();
-  
+  const [touched, setTouched] = useState(false);
+  const canPlaySound = useRef(false);
+
   const init = async () => {
     console.log('init...');
     await setupCamera();
@@ -36,6 +37,8 @@ function App() {
 
     console.log('setup done');
     console.log("Don't your touch face and press train 1");
+
+    initNotifications({ cooldown: 3000 });
   };
 
   const setupCamera = () => {
@@ -70,7 +73,6 @@ const train = async label => {
 }
 
 const training = label => {
-  
   return new Promise(async resolve => {
     const embedding = mobilenet.current.infer(
       video.current,
@@ -82,6 +84,34 @@ const training = label => {
   });
 }
 
+const run = async () => {
+  const embedding = mobilenet.current.infer(
+    video.current,
+    true
+  );
+  const result = await classifier.current.predictClass(embedding);
+  
+  if (
+    result.label === TOUCHED_LABEL &&
+    result.confidences[result.label] > TOUCHED_CONFIDENCE
+  ) {
+    console.log('Touched');
+    if (canPlaySound.current) {
+      canPlaySound.current = false;
+      sound.play();
+    }
+    notify('Touch hand', { body: 'heiiii, you touched face!!!' });
+    setTouched(true);
+  } else {
+    console.log('Not touch');
+    setTouched(false);
+  }
+
+  await sleep(200);
+
+  run();
+}
+
 const sleep = (ms = 0) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -89,12 +119,17 @@ const sleep = (ms = 0) => {
   useEffect(() => {
     init();
 
+    sound.on('end', function() {
+      canPlaySound.current = true;
+    });    
+
     // cleanup
     return () => {}
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div className="main">
+    <div className={`main ${touched ? 'touched' : ''}`}>
        <video
         ref= {video}
         className="video"
@@ -104,7 +139,7 @@ const sleep = (ms = 0) => {
       <div className="control">
         <button className='btn' onClick={() => train(NOT_TOUCH_LABEL)}>Train 1</button>
         <button className='btn' onClick={() => train(TOUCHED_LABEL)}>Train 2</button>
-        <button className='btn' onClick={() => {}}>Run</button>
+        <button className='btn' onClick={() => run()}>Run</button>
       </div>
     
     </div>
